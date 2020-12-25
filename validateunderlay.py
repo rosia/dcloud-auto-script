d917e19e597bb4f911add07ad0c5eca36e43a814
Rodney Sia <rosia@cisco.com>
2020-12-26 09:10:29 +1100

    added paramiko

--- validateunderlay.py
+++ validateunderlay.py
@@ -1,5 +1,9 @@
-import base64
 import paramiko
-# new from branch
-# Validate script using SSH and Paramikocd
-# new to test git diff
+from paramiko import SSHClient
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Auto Accepts SSH host keys from devices
+client.connect('10.66.50.135', username='dna-8', password='cisco!123')
+ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("show ip route")
+output = ssh_stdout.readlines()
+for line in output:
+    print(line.rstrip())
